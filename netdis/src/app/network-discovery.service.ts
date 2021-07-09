1cccabbef7f5a29c921842243a34bfcc1b899d28
David Leblanc <lblancdavid@gmail.com>
2021-07-09 10:52:51 -0700

    Sorta works but not really...

--- netdis/src/app/network-discovery.service.ts
+++ netdis/src/app/network-discovery.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { forkJoin, Observable } from 'rxjs';
-import { collectExternalReferences } from '@angular/compiler';
 
 export class NetworkedDevice {
   constructor(public ipAddress: string,
@@ -23,6 +22,7 @@ export class DeviceVersion {
 })
 export class NetworkDiscoveryService {
 
+  devices = new Array<NetworkedDevice>();
   constructor(private electronService: ElectronService,
     private http: HttpClient) { }
 
@@ -31,32 +31,25 @@ export class NetworkDiscoveryService {
 
     return new Observable<NetworkedDevice[]>((observer : any) => {
 
-      let devices = new Array<NetworkedDevice>();
       if(this.electronService.isElectronApp) {
+        this.devices = [];
         const netList =  this.electronService.remote.require('network-list');
         netList.scan({}, (err: any, arr: any[]) => {
-          forkJoin(arr.map(x => this.checkForSoftware(x.ip).subscribe(device => {
-            if(device) {
-              console.log(device);
-              devices.push(device);
-            }
-          }, error => {
-            console.error(error);
-          }
-          ))).subscribe(done => {
-            observer.next(devices);
+          console.log('scanning network...');
+          forkJoin(arr.map(x => this.checkForSoftware(x.ip))).subscribe(done => {
+            observer.next(this.devices);
             observer.complete();
           }, error => {
             
             console.error(error);
-            observer.next(devices);
+            observer.next(this.devices);
             observer.complete();
           });
         });
       }
       else {
         
-        observer.next(devices);
+        observer.next(this.devices);
         observer.complete();
       }
       
@@ -67,10 +60,13 @@ export class NetworkDiscoveryService {
   private checkForSoftware(ip: string): Observable<NetworkedDevice> {
     return new Observable<any>(observer => {
       this.http.get<DeviceVersion[]>('http://' + ip + ':4601/api/v1/softwareVersions').subscribe((versions : any) => {
-        observer.next(new NetworkedDevice(ip, versions));
+        var d = new NetworkedDevice(ip, versions);
+        this.devices.push(d);
+        observer.next(d);
         observer.complete();
       },
       error => {
+        observer.next(null);
         observer.complete();
       });
     });
